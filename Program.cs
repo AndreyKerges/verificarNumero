74c79a408b8f7c1965ed0a178279969dd7df53b8
Andrey Kerges Nascimento <andrey.nascimento2@fatec.sp.gov.br>
2024-09-03 22:55:33 -0300

    Exercício Finalizado

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿int num;
+Console.WriteLine("--- Verificar Número ---");
+
+Console.WriteLine("Digite um número: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+if (num == 5)
+{
+    Console.WriteLine("O número é igual a 5");
+}
+else if (num == 200)
+{
+    Console.WriteLine("O número é igual a 200");
+}
+else if (num == 400)
+{
+    Console.WriteLine("O número é igual a 400");
+}
+else if (num >= 500 && num <=1000)
+{
+    Console.WriteLine("O número esta entre o intervalo de 500 e 1000");
+}
+else
+{
+    Console.WriteLine("O número não esta nos intervalos anteriores");
+}
